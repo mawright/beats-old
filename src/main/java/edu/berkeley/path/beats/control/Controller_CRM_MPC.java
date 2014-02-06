8e8aff04022a9a58db9112e9d2d841292d453760
Gabriel Gomes <gomes@path.berkeley.edu>
2014-02-06 10:02:14 -0800

    fix pom

--- src/main/java/edu/berkeley/path/beats/control/Controller_CRM_MPC.java
+++ src/main/java/edu/berkeley/path/beats/control/Controller_CRM_MPC.java
@@ -1,7 +1,7 @@
 package edu.berkeley.path.beats.control;
 
 import edu.berkeley.path.beats.actuator.ActuatorRampMeter;
-import edu.berkeley.path.beats.control.adjoint_glue.AdjointRampMeteringPolicyMaker;
+//import edu.berkeley.path.beats.control.adjoint_glue.AdjointRampMeteringPolicyMaker;
 import edu.berkeley.path.beats.jaxb.ScenarioElement;
 import edu.berkeley.path.beats.simulator.*;
 import edu.berkeley.path.beats.simulator.Actuator;
@@ -67,9 +67,9 @@ public class Controller_CRM_MPC extends Controller {
                 case tester:
                     policy_maker = new PolicyMaker_Tester();
                     break;
-				case adjoint:
-					policy_maker = new AdjointRampMeteringPolicyMaker();
-					break;
+//				case adjoint:
+//					policy_maker = new AdjointRampMeteringPolicyMaker();
+//					break;
 //				case actm_lp:
 //                    policy_maker = new PolicyMaker_CRM_ACTM_LP();
 //					break;
---
+++ test-jar.bat
@@ -0,0 +1,1 @@
+java -jar beats-0.1-SNAPSHOT.one-jar.jar C:\Users\gomes\Dropbox\680_Data\config\680N\680N_v4_control.xml 5 test text 0 3600 300
