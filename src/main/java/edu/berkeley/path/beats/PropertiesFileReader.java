c7fea67f49a138d410f5f335bd516166f4a8f359
Gabriel Gomes <gomes@path.berkeley.edu>
2014-02-19 14:53:31 -0800

    asdf

---
+++ src/main/java/edu/berkeley/path/beats/PropertiesFileReader.java
@@ -0,0 +1,63 @@
+package edu.berkeley.path.beats;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class PropertiesFileReader {
+
+//    private static final Properties properties;
+//
+//    /** Use a static initializer to read from file. */
+//    static {
+//        InputStream inputStream = PropertiesFileReader.class.getResourceAsStream("/buildNumber.properties");
+//        properties = new Properties();
+//        try {
+//            properties.load(inputStream);
+//        } catch (IOException e) {
+//            throw new RuntimeException("Failed to read properties file", e);
+//        } finally {
+//            if (inputStream != null) {
+//                try {
+//                    inputStream.close();
+//                } catch (IOException e) {
+//                    // Ignore
+//                }
+//            }
+//        }
+//    }
+
+
+    public static void main(String[] args) {
+
+        InputStream inputStream = PropertiesFileReader.class.getResourceAsStream("/buildNumber.properties");
+        Properties properties = new Properties();
+        try {
+            properties.load(inputStream);
+            System.out.println("git-sha-1 = " + properties.getProperty("git-sha-1"));
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to read properties file", e);
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    // Ignore
+                }
+            }
+        }
+
+
+    }
+
+    /** Hide default constructor. */
+    private PropertiesFileReader() {}
+
+    /**
+     * Gets the Git SHA-1.
+     * @return A {@code String} with the Git SHA-1.
+     */
+//    public static String getGitSha1() {
+//        return properties.getProperty("git-sha-1");
+//    }
+}
