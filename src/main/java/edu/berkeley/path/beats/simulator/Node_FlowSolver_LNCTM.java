5a3d308f0980595985e132c2eabb29d2605846d9
Gabriel Gomes <gomes@path.berkeley.edu>
2014-06-10 11:24:54 -0700

    fix

--- src/main/java/edu/berkeley/path/beats/simulator/Node_FlowSolver_LNCTM.java
+++ src/main/java/edu/berkeley/path/beats/simulator/Node_FlowSolver_LNCTM.java
@@ -29,8 +29,8 @@ package edu.berkeley.path.beats.simulator;
 public class Node_FlowSolver_LNCTM extends Node_FlowSolver {
 
     // used in update()
-	protected double [][] outDemandKnown;	// [ensemble][nOut]
-	protected double [][] dsratio;			// [ensemble][nOut]
+	protected double [] outDemandKnown;	// [nOut]
+	protected double [] dsratio;			// [nOut]
 	protected boolean [][] iscontributor;	// [nIn][nOut]
 
 	/////////////////////////////////////////////////////////////////////
@@ -47,19 +47,17 @@ public class Node_FlowSolver_LNCTM extends Node_FlowSolver {
 
 	@Override
 	protected void reset() {
-    	int numEnsemble = myNode.getMyNetwork().getMyScenario().getNumEnsemble();	
-		iscontributor = new boolean[myNode.nIn][myNode.nOut];	
-		dsratio 		= new double[numEnsemble][myNode.nOut];
-		outDemandKnown 	= new double[numEnsemble][myNode.nOut];
+		iscontributor = new boolean[myNode.nIn][myNode.nOut];
+		dsratio 		= new double[myNode.nOut];
+		outDemandKnown 	= new double[myNode.nOut];
 	}
 	
 	@Override
-	protected IOFlow computeLinkFlows(final Double3DMatrix sr,final SupplyDemand demand_supply){
+	protected IOFlow computeLinkFlows(final Double3DMatrix sr,final SupplyDemand demand_supply,final int ensemble_index){
 
-    	int e,i,j,k;
+    	int i,j,k;
 		int nIn = myNode.nIn;
 		int nOut = myNode.nOut;        
-    	int numEnsemble = myNode.myNetwork.getMyScenario().getNumEnsemble();
     	int numVehicleTypes = myNode.myNetwork.getMyScenario().getNumVehicleTypes();
     	    	
         // input i contributes to output j .............................
@@ -67,58 +65,52 @@ public class Node_FlowSolver_LNCTM extends Node_FlowSolver {
         	for(j=0;j<sr.getnOut();j++)
         		iscontributor[i][j] = sr.getSumOverTypes(i,j)>0;
 	
-        double [][] applyratio = new double[numEnsemble][nIn];
+        double [] applyratio = new double[nIn];
 
-        for(e=0;e<numEnsemble;e++)
-	        for(i=0;i<nIn;i++)
-	        	applyratio[e][i] = Double.NEGATIVE_INFINITY;
+        for(i=0;i<nIn;i++)
+            applyratio[i] = Double.NEGATIVE_INFINITY;
         
-        for(e=0;e<numEnsemble;e++){
-	        for(j=0;j<nOut;j++){
-	        	
-	        	// re-compute known output demands .........................
-				outDemandKnown[e][j] = 0d;
-	            for(i=0;i<nIn;i++)
-	            	for(k=0;k<numVehicleTypes;k++)
-	            		outDemandKnown[e][j] += demand_supply.getDemand(e,i,k)*sr.get(i,j,k);
-	            
-	            // compute and sort output demand/supply ratio .............
-	            if(BeatsMath.greaterthan(demand_supply.getSupply(e,j),0d))
-	            	dsratio[e][j] = Math.max( outDemandKnown[e][j] / demand_supply.getSupply(e,j) , 1d );
-	            else
-	            	dsratio[e][j] = 1d;
-	            
-	            // reflect ratios back on inputs
-	            for(i=0;i<nIn;i++)
-	            	if(iscontributor[i][j])
-	            		applyratio[e][i] = Math.max(dsratio[e][j],applyratio[e][i]);
-	            	
-	        }
+        for(j=0;j<nOut;j++){
+
+            // re-compute known output demands .........................
+            outDemandKnown[j] = 0d;
+            for(i=0;i<nIn;i++)
+                for(k=0;k<numVehicleTypes;k++)
+                    outDemandKnown[j] += demand_supply.getDemand(i,k)*sr.get(i,j,k);
+
+            // compute and sort output demand/supply ratio .............
+            if(BeatsMath.greaterthan(demand_supply.getSupply(j),0d))
+                dsratio[j] = Math.max( outDemandKnown[j] / demand_supply.getSupply(j) , 1d );
+            else
+                dsratio[j] = 1d;
+
+            // reflect ratios back on inputs
+            for(i=0;i<nIn;i++)
+                if(iscontributor[i][j])
+                    applyratio[i] = Math.max(dsratio[j],applyratio[i]);
+
         }
+
         
-        IOFlow ioflow = new IOFlow(numEnsemble,nIn,nOut,numVehicleTypes);
+        IOFlow ioflow = new IOFlow(nIn,nOut,numVehicleTypes);
 
         // scale down input demands
-        for(e=0;e<numEnsemble;e++){
-	        for(i=0;i<nIn;i++){
-	            for(k=0;k<numVehicleTypes;k++){
-	            	ioflow.setIn(e,i,k , demand_supply.getDemand(e,i,k) / applyratio[e][i] );
-	            }
-	        }
+        for(i=0;i<nIn;i++){
+            for(k=0;k<numVehicleTypes;k++){
+                ioflow.setIn(i,k , demand_supply.getDemand(i,k) / applyratio[i] );
+            }
         }
-        
+
         // compute out flows ...........................................   
-        for(e=0;e<numEnsemble;e++){
-	        for(j=0;j<nOut;j++){
-	        	for(k=0;k<numVehicleTypes;k++){
-	        		double val = 0d;
-	            	for(i=0;i<nIn;i++)
-	            		val += ioflow.getIn(e,i,k)*sr.get(i,j,k);
-	            	ioflow.setOut(e,j,k,val);
-	        	}
-	        }
+        for(j=0;j<nOut;j++){
+            for(k=0;k<numVehicleTypes;k++){
+                double val = 0d;
+                for(i=0;i<nIn;i++)
+                    val += ioflow.getIn(i,k)*sr.get(i,j,k);
+                ioflow.setOut(j,k,val);
+            }
         }
-        
+
         return ioflow;
 	}
 
