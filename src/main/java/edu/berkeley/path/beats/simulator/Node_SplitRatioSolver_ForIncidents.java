f3e08f8d5f8a3d338eca0ba62f7099533ebf9c5d
Rasmus Ringdahl <Rasri660@student.liu.se>
2014-06-10 11:12:03 -0700

    Added - Validation method.

--- src/main/java/edu/berkeley/path/beats/simulator/Node_SplitRatioSolver_ForIncidents.java
+++ src/main/java/edu/berkeley/path/beats/simulator/Node_SplitRatioSolver_ForIncidents.java
@@ -7,9 +7,9 @@ public class Node_SplitRatioSolver_ForIncidents extends Node_SplitRatioSolver {
 	/* Fields */
 	private double treshold = 33.4544 ;//TODO Remove hardcoded value
     private double scaling_factor = 0.0004; //TODO Remove hardcoded value
-    private int fwy_id;
-    private int off_ramp_id;
-	
+    private int fwy_id = -1;
+    private int off_ramp_id = -1;
+
 	/* Constructor */
 	public Node_SplitRatioSolver_ForIncidents(Node myNode) {
 		super(myNode);
@@ -21,20 +21,19 @@ public class Node_SplitRatioSolver_ForIncidents extends Node_SplitRatioSolver {
 			{
 				fwy_id = n;
 			}
-			else
+			else if (myNode.output_link[n].getLinkType().getId() == 4 || myNode.output_link[n].getLinkType().getId() == 5)
 			{
 				off_ramp_id = n;
 			}
 		}		
 	}
-
+		
 	/* Methods */
 	// Method for adjust the split ratio
 	@Override
 	protected Double3DMatrix computeAppliedSplitRatio(Double3DMatrix splitratio_selected, SupplyDemand demand_supply) {
 		
-		
-		
+		// sr_predict = sr_local_avg + K * max(density - thresh, 0)
 		
 		
 		// TODO Auto-generated method stub
@@ -46,6 +45,32 @@ public class Node_SplitRatioSolver_ForIncidents extends Node_SplitRatioSolver {
 	protected void reset() {
 		// TODO Auto-generated method stub
 
+	}
+	
+	// Validation method
+	@Override
+	protected void validate() {
+		// Validation of incoming links
+		if(myNode.input_link.length != 1)
+		{
+			BeatsErrorLog.addError("Incorrect number of incomming links at node ID=" + myNode.getId() + " , total number of incomming links are " + myNode.input_link.length + " it must be 1.");
+		}
+		
+		// Validation of outgoing links
+		if(myNode.output_link.length != 2)
+		{
+			BeatsErrorLog.addError("Incorrect number of incomming links at node ID=" + myNode.getId() + " , total number of incomming links are " + myNode.output_link.length + " it must be 2.");
+		}
+		
+		if(fwy_id == -1)
+		{
+			BeatsErrorLog.addError("Missing downstream link of type Freeway at node ID=" + myNode.getId() + " ,  it must be exactly one link downstream of type Freeway.");
+		}
+		
+		if( off_ramp_id == -1)
+		{
+			BeatsErrorLog.addError("Missing diverging link of type Off-ramp/Interconnect at node ID=" + myNode.getId() + " ,  it must be exactly one diverging link of type Off-ramp or Interconnect.");
+		}
 	}
 
 }
