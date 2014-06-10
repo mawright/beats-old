a54b94a0279bb1fc78dfcd1621ec792e1f5470cc
Rasmus Ringdahl <Rasri660@student.liu.se>
2014-06-10 09:58:40 -0700

    Added - functionality for identifying links

--- src/main/java/edu/berkeley/path/beats/simulator/Node_SplitRatioSolver_ForIncidents.java
+++ src/main/java/edu/berkeley/path/beats/simulator/Node_SplitRatioSolver_ForIncidents.java
@@ -6,22 +6,42 @@ public class Node_SplitRatioSolver_ForIncidents extends Node_SplitRatioSolver {
 	
 	/* Fields */
 	private double treshold = 33.4544 ;//TODO Remove hardcoded value
-    private double scalingFactor = 0.0004; //TODO Remove hardcoded value
+    private double scaling_factor = 0.0004; //TODO Remove hardcoded value
+    private int fwy_id;
+    private int off_ramp_id;
 	
 	/* Constructor */
 	public Node_SplitRatioSolver_ForIncidents(Node myNode) {
 		super(myNode);
-		// TODO Auto-generated constructor stub
+		
+		// Find diverging link
+		for (int n = 0 ; n < myNode.output_link.length ; n++)
+		{
+			if (myNode.output_link[n].getLinkType().getId() == 1)
+			{
+				fwy_id = n;
+			}
+			else
+			{
+				off_ramp_id = n;
+			}
+		}		
 	}
 
-	/* Adjustment of split ratio */
+	/* Methods */
+	// Method for adjust the split ratio
 	@Override
 	protected Double3DMatrix computeAppliedSplitRatio(Double3DMatrix splitratio_selected, SupplyDemand demand_supply) {
+		
+		
+		
+		
+		
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	/* Reset function */
+	// Reset method
 	@Override
 	protected void reset() {
 		// TODO Auto-generated method stub
