8c564d3de9a36e581bf85a40c8841d650d88bb2d
mawright <mwright@berkeley.edu>
2015-10-05 14:55:45 -0700

    more WIP

--- src/main/java/edu/berkeley/path/beats/simulator/nodeBeahavior/Node_SplitRatioSolver_Balancing.java
+++ src/main/java/edu/berkeley/path/beats/simulator/nodeBeahavior/Node_SplitRatioSolver_Balancing.java
@@ -1,9 +1,8 @@
 package edu.berkeley.path.beats.simulator.nodeBeahavior;
 
 import edu.berkeley.path.beats.simulator.Node;
-import edu.berkeley.path.beats.simulator.output.InterfaceOutputWriter;
+import edu.berkeley.path.beats.simulator.utils.BeatsMath;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,6 +23,11 @@ public class Node_SplitRatioSolver_Balancing extends Node_SplitRatioSolver{
 
 	protected List<Integer>[] U_j; // links i that wish to send to link j
 	protected List<Integer>[][] V_ic; // links j to which (i,c) has undefined split ratios
+	protected List<Integer> V; // links j to which split may still be assigned
+
+	protected double[][] unallocated_demand;
+	protected double[][][] oriented_demand;
+	protected double[][] oriented_priority;
 
 	private final double zeroThreshold = Double.MIN_VALUE * 2;
 
@@ -39,6 +43,9 @@ public class Node_SplitRatioSolver_Balancing extends Node_SplitRatioSolver{
 	@Override
 	public void reset() {
 		nVType = myNode.getMyNetwork().getMyScenario().get.numVehicleTypes();
+		unallocated_demand = new double[myNode.nIn][nVType];
+		oriented_demand = new double[myNode.nIn][myNode.nOut][nVType];
+		oriented_priority = new double[myNode.nIn][myNode.nOut];
 	}
 
 	@Override
@@ -51,16 +58,22 @@ public class Node_SplitRatioSolver_Balancing extends Node_SplitRatioSolver{
 		}
 
 		initializeAssignedSplits(splitratio_selected);
-		assignZeroDemandSplits(splitratio_selected);
+		assignZeroDemandSplits();
 		assignZeroRemainingSplits();
 
 		initializeSets();
 		regularizePriorities();
 
+		while(!allSplitsSolved()) {
+			calculateRemainingDemand();
+			calculateOrientedDemand();
+			calculateOrientedPriorities(splitratio_selected);
+		}
+
 		return computed_splitratio;
 	}
 
-	private void initializeAssignedSplits(final Double[][][] splitratio_selected) {
+	private void initializeAssignedSplits(final Double[][][] input_splitratio) {
 		for(int i=0;i<myNode.nIn;i++) {
 			for(int c=0;c<nVType;c++) {
 				// initialize counter of how much split remains to be assigned assignment
@@ -68,20 +81,21 @@ public class Node_SplitRatioSolver_Balancing extends Node_SplitRatioSolver{
 
 				for(int j=0;j<myNode.nOut;j++) {
 
-					if (Double.isNaN(splitratio_selected[i][j][c])) {
+					if (Double.isNaN(input_splitratio[i][j][c])) {
 						splitKnown[i][j][c] = false;
 					}
 					else {
 						splitKnown[i][j][c] = true;
-						splitRemaining[i][c] = splitRemaining[i][c] - splitratio_selected[i][j][c];
+						splitRemaining[i][c] = splitRemaining[i][c] - input_splitratio[i][j][c];
+						computed_splitratio[i][j][c] = input_splitratio[i][j][c];
 					}
 				}
 			}
 		}
 	}
 
-	private void assignZeroDemandSplits(final Double [][][] splitratio_selected) {
-		for (int i=0;i<splitratio_selected.length;i++){
+	private void assignZeroDemandSplits() {
+		for (int i=0;i<myNode.nIn;i++){
 			for (int c=0;c<nVType;c++){
 				if (demands[i][c] < zeroThreshold) { // splits may be assigned arbitrarily - assign 1 to first j
 					computed_splitratio[i][0][c] = 1d;
@@ -114,6 +128,8 @@ public class Node_SplitRatioSolver_Balancing extends Node_SplitRatioSolver{
 	private void initializeSets() {
 		int i,j,c;
 
+		V = new ArrayList<Integer>(myNode.nOut);
+
 		for(j=0;j<myNode.nOut;j++){
 			U_j[j] = new ArrayList<Integer>(myNode.nIn);
 
@@ -126,6 +142,8 @@ public class Node_SplitRatioSolver_Balancing extends Node_SplitRatioSolver{
 					}
 				}
 			}
+			if(!U_j[j].isEmpty())
+				V.add(j);
 		}
 
 		for(i=0;i<myNode.nIn;i++){
@@ -162,6 +180,50 @@ public class Node_SplitRatioSolver_Balancing extends Node_SplitRatioSolver{
 		}
 	}
 
+	private boolean allSplitsSolved() {
+		return V.isEmpty();
+	}
+
+	private void calculateRemainingDemand() {
+		for (int i = 0; i < myNode.nIn; i++) {
+			for (int c=0; c < nVType; c++) {
+				unallocated_demand[i][c] = splitRemaining[i][c] * demands[i][c];
+			}
+		}
+	}
+
+	private void calculateOrientedDemand() {
+		for (int i = 0; i < myNode.nIn; i++) {
+			for (int j=0; j<myNode.nOut; j++) {
+				for (int c=0; c < nVType; c++) {
+					oriented_demand[i][j][c] = computed_splitratio[i][j][c] * demands[i][c];
+				}
+			}
+		}
+	}
+
+	private void calculateOrientedPriorities(final Double[][][] input_splitratio) {
+		double[] gamma = new double[nVType];
+		double[] numerator = new double[nVType];
+		for (int i = 0; i<myNode.nIn; i++) {
+			for( int j=0; j<myNode.nOut; j++) {
+				for( int c=0; c<nVType; c++) {
+					if(Double.isNaN(input_splitratio[i][j][c]))
+						gamma[c] = input_splitratio[i][j][c];
+					else
+						gamma[c] = computed_splitratio[i][j][c] + splitRemaining[i][c] / V_ic[i][c].size();
+
+					numerator[c] = gamma[c] * demands[i][c];
+				}
+
+				oriented_priority[i][j] = inputPriorities[i] * BeatsMath.sum(numerator) / BeatsMath.sum(demands[i]);
+			}
+		}
+	}
+
+	private void findLargestOrientedDSRatio() {
+		
+	}
 
 	private ArrayList<Integer> makeTuple(int i, int j, int c) {
 		ArrayList<Integer> output = new ArrayList<Integer>(3);
