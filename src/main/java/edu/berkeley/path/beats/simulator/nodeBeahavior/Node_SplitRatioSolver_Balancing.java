6f961fccf056711b95072a4b9cbb24ac49ab05f5
mawright <mwright@berkeley.edu>
2015-10-05 12:48:22 -0700

    WIP on balancing split assignment

--- src/main/java/edu/berkeley/path/beats/simulator/nodeBeahavior/Node_SplitRatioSolver_Balancing.java
+++ src/main/java/edu/berkeley/path/beats/simulator/nodeBeahavior/Node_SplitRatioSolver_Balancing.java
@@ -1,6 +1,11 @@
 package edu.berkeley.path.beats.simulator.nodeBeahavior;
 
 import edu.berkeley.path.beats.simulator.Node;
+import edu.berkeley.path.beats.simulator.output.InterfaceOutputWriter;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by matt on 9/23/15.
@@ -8,6 +13,17 @@ import edu.berkeley.path.beats.simulator.Node;
  * "A new model for multi-commodity macroscopic modeling of complex traffic networks"
  */
 public class Node_SplitRatioSolver_Balancing extends Node_SplitRatioSolver{
+
+	protected Double [][][] computed_splitratio; //nIn x nOut x nVType
+	protected int nVType;
+	protected Double [][] demands;
+	protected double [] inputPriorities;
+
+	protected boolean [][][] splitKnown;
+	protected Double [][] splitRemaining; // nIn x nVType
+
+	protected List<Integer>[] U_j; // links i that wish to send to link j
+	protected List<Integer>[][] V_ic; // links j to which (i,c) has undefined split ratios
 
 	public Node_SplitRatioSolver_Balancing(Node myNode) {
 		super(myNode);
@@ -20,13 +36,121 @@ public class Node_SplitRatioSolver_Balancing extends Node_SplitRatioSolver{
 
 	@Override
 	public void reset() {
-
+		nVType = myNode.getMyNetwork().getMyScenario().get.numVehicleTypes();
 	}
 
 	@Override
 	public Double [][][] computeAppliedSplitRatio(final Double [][][] splitratio_selected,final int e) {
 
-		return null;
+		// get link information
+		inputPriorities = myNode.getInputLinkPriorities(e);
+		for(int i=0;i<myNode.getnIn();i++){
+			demands[i] = myNode.getInput_link()[i].get_out_demand_in_veh(e);
+		}
+
+		initializeAssignedSplits(splitratio_selected);
+		assignZeroDemandSplits(splitratio_selected);
+		assignZeroRemainingSplits();
+
+		initializeSets();
+
+		return computed_splitratio;
+	}
+
+	private void initializeAssignedSplits(final Double[][][] splitratio_selected) {
+		for(int i=0;i<myNode.nIn;i++) {
+			for(int c=0;c<nVType;c++) {
+				// initialize counter of how much split remains to be assigned assignment
+				splitRemaining[i][c] = 1d;
+
+				for(int j=0;j<myNode.nOut;j++) {
+
+					if (Double.isNaN(splitratio_selected[i][j][c])) {
+						splitKnown[i][j][c] = false;
+					}
+					else {
+						splitKnown[i][j][c] = true;
+						splitRemaining[i][c] = splitRemaining[i][c] - splitratio_selected[i][j][c];
+					}
+				}
+			}
+		}
+	}
+
+	private void assignZeroDemandSplits(final Double [][][] splitratio_selected) {
+		double threshold = Double.MIN_VALUE * 2;
+		for (int i=0;i<splitratio_selected.length;i++){
+			for (int c=0;c<nVType;c++){
+				if (demands[i][c] < threshold) { // splits may be assigned arbitrarily - assign 1 to first j
+					computed_splitratio[i][0][c] = 1d;
+					splitRemaining[i][c] = 0d;
+					splitKnown[i][0][c] = true;
+
+					for(int j=1;j<myNode.nOut;j++){
+						computed_splitratio[i][j][c] = 0d;
+						splitKnown[i][j][c] = true;
+					}
+				}
+			}
+		}
+	}
+
+	private void assignZeroRemainingSplits() {
+		int i,j,c;
+		double threshold = Double.MIN_VALUE * 2;
+		for(i=0;i<myNode.nIn;i++){
+			for(c=0;c<nVType;c++){
+				if (splitRemaining[i][c] < threshold) {
+					for(j=0;j<myNode.nOut;j++) {
+						splitKnown[i][j][c] = true;
+						computed_splitratio[i][j][c] = 0d;
+					}
+				}
+			}
+		}
+	}
+
+	private void initializeSets() {
+		int i,j,c;
+
+		for(j=0;j<myNode.nOut;j++){
+			U_j[j] = new ArrayList<Integer>(myNode.nIn);
+
+			outerloop1:
+			for(i=0;i<myNode.nIn;i++){
+				for(c=0;c<nVType;c++){
+					if (!splitKnown[i][j][c]) {
+						U_j[j].add(i);
+						break outerloop1;
+					}
+				}
+			}
+		}
+
+		for(i=0;i<myNode.nIn;i++){
+			for(c=0;c<nVType;c++){
+				V_ic[i][c] = new ArrayList<Integer>(myNode.nOut);
+				for(j=0;j<myNode.nOut;j++){
+					if (U_j[j].contains(i))
+						V_ic[i][c].add(j);
+				}
+				if (V_ic[i][c].size()==1) {
+					j = V_ic[i][c].get(0);
+					splitKnown[i][j][c] = true;
+					computed_splitratio[i][j][c] = splitRemaining[i][c];
+					splitRemaining[i][c] = 0d;
+				}
+			}
+		}
+	}
+
+
+	private ArrayList<Integer> makeTuple(int i, int j, int c) {
+		ArrayList<Integer> output = new ArrayList<Integer>(3);
+		output.add(0,i);
+		output.add(1,j);
+		output.add(2,c);
+		return output;
 	}
 
 }
